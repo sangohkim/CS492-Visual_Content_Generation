7000944e8cc927e4985fa32970d6d0a69369634c
sangohkim <tkddh1109@kaist.ac.kr>
2025-11-18 03:59:38 +0000

    feat: add i2i for baseline

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,9 +5,9 @@ OUTPUT_ROOT="./results/dreambooth-sdxl"
 
 SEED=42
 BATCH_SIZE=1
-NUM_TRAIN_EPOCHS=100
+NUM_TRAIN_EPOCHS=1000
 CKPT_STEP=100
-LR=1e-4
+LR=5e-5
 
 accelerate launch train_text_to_image_lora_sdxl.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -15,7 +15,6 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --caption_column="text" \
   --resolution=1024 \
   --train_data_dir=$DATA_ROOT \
-  --random_flip \
   --train_batch_size=$BATCH_SIZE \
   --num_train_epochs=$NUM_TRAIN_EPOCHS \
   --checkpointing_steps=$CKPT_STEP \
@@ -25,6 +24,6 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --mixed_precision="fp16" \
   --seed=$SEED \
   --output_dir="$OUTPUT_ROOT/$(basename $DATA_ROOT)" \
-  --validation_epochs 50 \
+  --validation_epochs 10 \
   --validation_prompt="A sks monster jumping on the moon" \
   --report_to="wandb" \
