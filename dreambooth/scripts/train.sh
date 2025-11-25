c11bef5b23591bceec0471f27274a1ac0efe19f7
sangohkim <tkddh1109@kaist.ac.kr>
2025-11-25 00:47:13 +0000

    chore: minor changes

--- dreambooth/scripts/train.sh
+++ dreambooth/scripts/train.sh
@@ -1,13 +1,14 @@
 MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
 VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
-DATA_ROOT="/root/sangoh/CS492-Visual_Content_Generation/dataset/monster_toy"
+DATA_ROOT="/root/sangoh/CS492-Visual_Content_Generation/dataset/nupjuki-cropped"
 OUTPUT_ROOT="./results/dreambooth-sdxl"
 
 SEED=42
 BATCH_SIZE=1
 NUM_TRAIN_EPOCHS=1000
 CKPT_STEP=100
-LR=5e-5
+LR=1e-4
+GRAD_ACC_STEPS=4
 
 accelerate launch train_text_to_image_lora_sdxl.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -18,6 +19,7 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --train_batch_size=$BATCH_SIZE \
   --num_train_epochs=$NUM_TRAIN_EPOCHS \
   --checkpointing_steps=$CKPT_STEP \
+  --gradient_accumulation_steps=$GRAD_ACC_STEPS \
   --learning_rate=$LR \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
@@ -25,5 +27,5 @@ accelerate launch train_text_to_image_lora_sdxl.py \
   --seed=$SEED \
   --output_dir="$OUTPUT_ROOT/$(basename $DATA_ROOT)" \
   --validation_epochs 10 \
-  --validation_prompt="A sks monster jumping on the moon" \
+  --validation_prompt="a sks plush jumping on the moon" \
   --report_to="wandb" \
